d4b89c9566558f3ced5e33a387f488f1ed4c88df
João Gabriel <joaogabrielamaral@outlook.com.br>
2017-10-27 10:07:46 -0200

    alteracao na geracao do excel

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,85 +1,104 @@
+var url = "https://api.trello.com";
+var key = "bffdd838fd24b5832de6f72e47987533";
+var token = "685daea308b5bff7e3d2da00807bcb34a12f1e24685928fc444acb0875d01429";
+
+
 document.addEventListener('DOMContentLoaded', function() {
     var checkPageButton = document.getElementById('cl');
     checkPageButton.addEventListener('click', function() {
-        //Para buscar todas listas
-        //https://api.trello.com/1/boards/Cm1VvpPQ?fields=id,name&lists=open&list_fields=id,name,closed,pos&key=175bab29d30ac5c2db6f9b9de3de5b4b&token=b7f75def67760a80d454cf834ff6414fc75d5f4342662927fd0cec6c0af5c9c8
-      $.get("https://api.trello.com/1/lists/5811dd0416356cda5996a190/cards?actions=commentCard&key=175bab29d30ac5c2db6f9b9de3de5b4b&token=b7f75def67760a80d454cf834ff6414fc75d5f4342662927fd0cec6c0af5c9c8",
-                function(datas){
-        /* original data */
-        var data = [
-                ["Nome card","comentarios",3],
-                [datas[0].actions[0].data.card.name, datas[0].actions[0].data.text, null],
-                [datas[1].actions[0].data.card.name, datas[1].actions[0].data.text,"0.3"], 
-                [datas[2].actions[0].data.card.name, datas[2].actions[0].data.text, "qux"]
-            ]
-        var ws_name = "SheetJS";
-        var reader = new FileReader();
-
-        function s2ab(s) {
-            if(typeof ArrayBuffer !== 'undefined') {
-                var buf = new ArrayBuffer(s.length);
-                var view = new Uint8Array(buf);
-                for (var i=0; i!=s.length; ++i) view[i] = s.charCodeAt(i) & 0xFF;
-                return buf;
-            } else {
-                var buf = new Array(s.length);
-                for (var i=0; i!=s.length; ++i) buf[i] = s.charCodeAt(i) & 0xFF;
-                return buf;
-            }
-        }
-
-        /* require XLSX */
-        //var XLSX = require('xlsx')
-        var X = XLSX;
-
-        /* set up workbook objects -- some of these will not be required in the future */
-        var wb = {}
-        wb.Sheets = {};
-        wb.Props = {};
-        wb.SSF = {};
-        wb.SheetNames = [];
-
-        /* create worksheet: */
-        var ws = {}
-
-        /* the range object is used to keep track of the range of the sheet */
-        var range = {s: {c:0, r:0}, e: {c:0, r:0 }};
-
-        /* Iterate through each element in the structure */
-        for(var R = 0; R != data.length; ++R) {
-            if(range.e.r < R) range.e.r = R;
-            for(var C = 0; C != data[R].length; ++C) {
-             if(range.e.c < C) range.e.c = C;
-
-            /* create cell object: .v is the actual data */
-                var cell = { v: data[R][C] };
-                if(cell.v == null) continue;
-
-            /* create the correct cell reference */
-                var cell_ref = X.utils.encode_cell({c:C,r:R});
-
-            /* determine the cell type */
-                if(typeof cell.v === 'number') cell.t = 'n';
-                else if(typeof cell.v === 'boolean') cell.t = 'b';
-                else cell.t = 's';
-
-            /* add to structure */
-            ws[cell_ref] = cell;
-        }
-        }
-        ws['!ref'] = X.utils.encode_range(range);
-
-        /* add worksheet to workbook */
-        wb.SheetNames.push(ws_name);
-        wb.Sheets[ws_name] = ws;
-
-        /* write file */
-        var wbout = XLSX.write(wb, {bookType:"xlsx", bookSST:true, type: 'binary'});
-        var blob = new Blob([s2ab(wbout)],{type:"application/octet-stream"});
-        var URL = window.URL || window.webkitURL;
-        var downloadUrl = URL.createObjectURL(blob);
-        chrome.downloads.download({url:downloadUrl, filename:"teste.xlsx"},function(id) { });
-    });
+        getColumns("cns49FIZ");
     }, false);
-  }, false);
+}, false);
 
+function s2ab(s) {
+    if(typeof ArrayBuffer !== 'undefined') {
+        var buf = new ArrayBuffer(s.length);
+        var view = new Uint8Array(buf);
+        for (var i=0; i!=s.length; ++i) view[i] = s.charCodeAt(i) & 0xFF;
+        return buf;
+    } else {
+        var buf = new Array(s.length);
+        for (var i=0; i!=s.length; ++i) buf[i] = s.charCodeAt(i) & 0xFF;
+        return buf;
+    }
+}
+
+function getColumns(idTrello){
+    //cns49FIZ
+    $.ajax({
+        url : url+"/1/boards/"+idTrello+"?fields=name&lists=open&key=" + key +"&token=" + token,
+        type : "get",
+        async: false,
+        success : (data) => {
+            if(!data) return;
+            data.lists.forEach(function(item) {
+                console.log(item);
+                getItensColum(item);
+            });
+        }
+    });
+}
+
+function getItensColum(colum){
+    $.ajax({
+        url : url + "/1/lists/"+ colum.id +"/cards?actions=commentCard&key=" + key +"&token=" + token,
+        type : "get",
+        async: false,
+        success : (data) => {
+            console.log(data);
+            // exportExcel(data, colum);
+        }
+    });
+}
+
+function exportExcel(itensColumns, colum){
+    var reader = new FileReader();
+    var xls = XLSX;
+    var ws = {};
+    var ws_name = colum.name;
+    var wb = {
+        "Sheets": {},
+        "Props": {},
+        "SSF": {},
+        "SheetNames": []
+    };
+
+    var cellRefTit = xls.utils.encode_cell({ c: 0, r: 0 });
+    ws[cellRefTit] = "Descrição";
+
+    cellRefTit = xls.utils.encode_cell({ c: 1, r: 0 });
+    ws[cellRefTit] = "Email enviado";
+
+    cellRefTit = xls.utils.encode_cell({ c: 2, r: 0 });
+    ws[cellRefTit] = "Orçamento enviado";
+
+    cellRefTit = xls.utils.encode_cell({ c: 3, r: 0 });
+    ws[cellRefTit] = "Escopo aprovado";
+    
+    var rang = { cell: 0, row: 0 };
+    itensColumns.forEach((itemColum) => {
+        //descri
+        var cell_ref = xls.utils.encode_cell({ c: 0, r: rang.row });
+        ws[cell_ref] = item.name;
+        
+        //teste
+        var tests = [
+            { r: /Email enviado/, cell: 1, qtd: 0 },
+            { r: /Orçamento enviado/, cell: 2, qtd: 0 },
+            { r: /Escopo aprovado/, cell: 3, qtd: 0 },
+            { r: /teste/, cell: 4, qtd: 0 }
+        ];
+
+        if(item.actions == null || item.actions.length == 0) continue;
+        item.actions.forEach((action) => {
+            tests.forEach((test) => {
+                if(test.exec(action.data.text).length){
+                    test.qtd++;
+                    //gambiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii
+                    var cellRefTest = xls.utils.encode_cell({ c: test.cell, r: rang.row });
+                    ws[cellRefTest] = test.qtd;
+                }
+            });
+        });
+    });
+}
